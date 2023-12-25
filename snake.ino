2a66b6e129929850f64d567fc9c9106010f9d1d2
jbgam <jbgamer17252@gmail.com>
2023-12-25 22:57:01 +0100

    - Finished snake movement - Added the snake tail - Added "food" for score - Score is tracked - Snake can collide with its tail - Game reset function - Game pauze function

--- snake.ino
+++ snake.ino
@@ -12,6 +12,13 @@ ArduinoLEDMatrix matrix;
 #define SW_PIN   2
 ezButton button(SW_PIN);
 
+// Snake tail variables
+int tailLength = 0;
+// Max amount of leds is 96 so max score can only be 95 to keep functioning
+const int maxTailLength = 95;
+int tailX[maxTailLength];
+int tailY[maxTailLength];
+
 // Board variables  
 byte matrixx[8][12];
 int xValue = 0;
@@ -19,76 +26,161 @@ int yValue = 0;
 int bValue = 0;
 int currentpositionX = 3;
 int currentpositionY = 5;
+bool foodExists = false;
+int foodXpos;
+int foodYpos;
+unsigned long previousMillis = 0;
+const long interval = 150;
 int directionX;
 int directionY;
+bool gamePaused = false;
+
+// Score variables
+int score;
+bool scoreAdded = false;
 
 void setup() {
   Serial.begin(9600);
+  button.setDebounceTime(50);
   matrix.begin();
   matrixx[3][5] = 1;
   matrix.renderBitmap(matrixx, 8, 12);
 }
 
 void loop() {
-  // Clear the board so it can be drawn again
-  clearMatrix();
+  button.loop();
+  // Check if the pauze button has been pressed to pauze or unpauze the game
+  gamePauze();
 
-  // read analog X and Y values of the joystick
-  xValue = analogRead(VRX_PIN);
-  yValue = analogRead(VRY_PIN);
-  // Read the joystick button value
-  bValue = button.getState();
+  // If the game is not pauzed
+  if (!gamePaused) {
+    // Clear the board so it can be drawn again
+    clearMatrix();
 
-  // Only update the direction if button press
-  if (bValue == HIGH) {
-    updateDirection(xValue, yValue);
+    // read analog X and Y values of the joystick
+    xValue = analogRead(VRX_PIN);
+    yValue = analogRead(VRY_PIN);
+    // Read the joystick button value
+    bValue = button.getState();
+
+    // If the snake has picked up a food item, make another one
+    if (foodExists == false) {
+      createFood();
+    }
+
+    // Check if the snake has collided with a food item
+    foodCollision();
+    
+    // Keep track of and draw the snake's tail
+    updateTail();
+    drawTail();
+
+    // Only update the direction if button press
+    if (bValue == HIGH) {
+      updateDirection(xValue, yValue);
+    }
+    // move snake in the direction its going
+    moveSnake();
+    // check if snake has collided with itself
+    selfCollisionDetection();
+    // light (the led) the current position
+    matrixx[currentpositionX][currentpositionY] = 1;
+
+    unsigned long currentMillis = millis();
+
+    // Blink food every so often
+    if (currentMillis - previousMillis >= interval) {
+      // Save blink time
+      previousMillis = currentMillis;
+      // Toggle the LED at food position
+      toggleLED(foodXpos, foodYpos);
+    }
+    // render the matrix
+    matrix.renderBitmap(matrixx, 8, 12);
+    // game update every 100ms
+    delay(100);
   }
-  // move the snake
-  moveSnake();
-  // light (the led of) the current position
-  matrixx[currentpositionX][currentpositionY] = 1;
-  // render the matrix
-  matrix.renderBitmap(matrixx, 8, 12);
-  // add game delay, a game update every 85 ms, so the snake moves and everything else
-  delay(85);
+}
+
+void Reset() {
+  // This function completely resets the "game state" every aspect of the game is reset
+  clearMatrix();
+  currentpositionX = 3;
+  currentpositionY = 5;
+  foodExists = false;
+  foodXpos = 0;
+  foodYpos = 0;
+  directionX = NULL;
+  directionY = NULL;
+  tailLength = 0;
+  score = 0;
+}
+
+void gamePauze() {
+  // Toggle the game pause state when the button is pressed
+  if (digitalRead(SW_PIN) == LOW) {
+    gamePaused = !gamePaused;
+    if (gamePaused) {
+      Serial.println("Game paused");
+    } else {
+      Serial.println("Game resumed");
+    }
+    while (digitalRead(SW_PIN) == LOW) {
+      // Wait for the button to be released
+      delay(10);
+    }
+  }
 }
 
 void updateDirection(int xValue, int yValue) {
-  // Update direction based on joystick input
-  if (xValue > 1020) {
+  // Update the direction of the snake based on joystick input
+  // It checks which direction is being pressed by reading x and y value of the controller
+  // Also checks if the snake is trying to back into itself
+  if (xValue > 1020 && directionX != 1) {
     directionX = -1;
-  } else if (xValue < 200) {
-    directionX = 1;
-  } else {
-    directionX = 0;
-  }
-
-  if (yValue > 1020) {
-    directionY = -1;
-  } else if (yValue < 200) {
-    directionY = 1;
-  } else {
     directionY = 0;
+  } else if (xValue < 200 && directionX != -1) {
+    directionX = 1;
+    directionY = 0;
+  } else if (yValue > 1020 && directionY != 1) {
+    directionX = 0;
+    directionY = -1;
+  } else if (yValue < 200 && directionY != -1) {
+    directionX = 0;
+    directionY = 1;
   }
 }
+
 void moveSnake() {
   // Move the snake continuously in the current direction
   currentpositionX += directionX;
   currentpositionY += directionY;
+
   // Keep the snake in bounds of matrix check
   checkPositionXY();
 }
 
+void selfCollisionDetection() {
+  for (int i = 0; i < tailLength; i++) {
+    // check if the head of the snake is in the same position as a tail element
+    if (currentpositionX == tailX[i] && currentpositionY == tailY[i]) {
+      // Reset the game; clear the board, variables etc
+      Reset();
+    }
+  }
+}
+
 void clearMatrix() {
+  // Reset every led in the matrix for next "frame"
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 12; j++) {
       matrixx[i][j] = 0;
     }
   }
-  matrix.renderBitmap(matrixx, 8, 12);
 }
 
 bool checkMatrix(int x, int y) {
+  // Check if the given matrix led is off
   if (matrixx[x][y] == 0) {
     return true;
   } else {
@@ -96,8 +188,24 @@ bool checkMatrix(int x, int y) {
   }
 }
 
+bool isSnakeSegment(int x, int y) {
+  // Check if the position is occupied by the snake's head
+  if (x == currentpositionX && y == currentpositionY) {
+    return true;
+  }
+
+  // Check if the position is occupied by any tail segment
+  for (int i = 0; i < tailLength; i++) {
+    if (tailX[i] == x && tailY[i] == y) {
+      return true;
+    }
+  }
+  // if occupied by neither, return false
+  return false;
+}
+
 void checkPositionXY() {
-  // Check if the snake has moved "outside of the led matrix grid", if so place them on the other side of the board
+  // Check if the snake has moved "outside of the led matrix grid", if so place them on the other side of the board back in
   if (currentpositionX > 7) {
     currentpositionX = 0;
   } else if (currentpositionX < 0) {
@@ -110,9 +218,69 @@ void checkPositionXY() {
   }
 }
 
-void toggleLED(int x, int y) {
-  // Toggle on or off
-  matrixx[x][y] = 1 - matrixx[x][y];
+void createFood() {
+  do {
+    // Generate random position for the "food" to be placed within the matrix
+    int randomX = random(7);
+    int randomY = random(11);
+    if (checkMatrix(randomX, randomY) && !isSnakeSegment(randomX, randomY)) {
+      foodXpos = randomX;
+      foodYpos = randomY;
+      matrixx[randomX][randomY] = 1;
+      foodExists = true;
+      break; // exit loop when spot found
+    }
+  } while (true); // continue until a empty spot is found (not already occupied by the snake)
+}
 
-  matrix.renderBitmap(matrixx, 8, 12);
+void foodCollision() {
+  // Check if the head of the snake is on the same x and y position as the food item
+  if (currentpositionX == foodXpos && currentpositionY == foodYpos) {
+    if (!scoreAdded) {
+      foodExists = false;
+      // When snake collides with "food," add another tail element and score
+      createTailSegment();
+      addScore();
+      // scoreAdded variable to prevent giving double score with fast loop times
+      scoreAdded = true;
+    }
+  } else {
+    // reset scoreAdded
+    scoreAdded = false;
+  }
+}
+
+void addScore() {
+  // add 1 score to total score
+  score++;
+}
+
+void createTailSegment() {
+  // check if not exceeding tail lenght limit
+  if (tailLength < maxTailLength) {
+    // Add one to tailLength (creating a tail element)
+    tailLength++;
+  }
+}
+
+void updateTail() {
+  // Move every tail element to the position of the tail element infront of them (moving the snake forward)
+  for (int i = tailLength - 1; i > 0; i--) {
+    tailX[i] = tailX[i - 1];
+    tailY[i] = tailY[i - 1];
+  }
+  // Set the position of the first tail element to the last position of the snake head
+  tailX[0] = currentpositionX;
+  tailY[0] = currentpositionY;
+}
+
+void drawTail() {
+  for (int i = 0; i < tailLength; i++) {
+    matrixx[tailX[i]][tailY[i]] = 1;
+  }
+}
+
+void toggleLED(int x, int y) {
+  // Toggle a led on or off (if on set off, if off set on)
+  matrixx[x][y] = 1 - matrixx[x][y];
 }
